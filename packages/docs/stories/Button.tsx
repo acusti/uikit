0b884f4c08eb793b0777526a65e4eb75889d98e4
Andrew Patton <andrew@acusti.ca>
2021-03-27 21:45:12 -0700

    Format storybook files with prettier

--- packages/docs/stories/Button.tsx
+++ packages/docs/stories/Button.tsx
@@ -7,45 +7,45 @@ import './button.css';
  */
 
 type Props = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean,
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string,
-  /**
-   * How large should the button be?
-   */
-  size: 'small' | 'medium'| 'large',
-  /**
-   * Button contents
-   */
-  label: string,
-  /**
-   * Optional click handler
-   */
-  onClick: () => mixed,
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary?: boolean;
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    /**
+     * How large should the button be?
+     */
+    size: 'small' | 'medium' | 'large';
+    /**
+     * Button contents
+     */
+    label: string;
+    /**
+     * Optional click handler
+     */
+    onClick: () => mixed;
 };
 
 export const Button = ({ primary, backgroundColor, size, label, ...props }: Props) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    return (
+        <button
+            type="button"
+            className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+        >
+            {label}
+        </button>
+    );
 };
 
 Button.defaultProps = {
-  backgroundColor: null,
-  onClick: undefined,
-  primary: false,
-  size: 'medium',
+    backgroundColor: null,
+    onClick: undefined,
+    primary: false,
+    size: 'medium',
 };
--- packages/docs/stories/Page.stories.js
+++ packages/docs/stories/Page.stories.js
@@ -4,18 +4,18 @@ import { Page } from './Page.tsx';
 import * as HeaderStories from './Header.stories.js';
 
 export default {
-  title: 'Example/Page',
-  component: Page,
+    title: 'Example/Page',
+    component: Page,
 };
 
 const Template = (args) => <Page {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  ...HeaderStories.LoggedIn.args,
+    ...HeaderStories.LoggedIn.args,
 };
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
+    ...HeaderStories.LoggedOut.args,
 };
