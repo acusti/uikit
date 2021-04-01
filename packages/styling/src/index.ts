3687407a20cc0841992517bf1986e49660ca36a3
Andrew Patton <andrew@acusti.ca>
2021-04-01 15:56:55 -0700

    Add SYSTEM_UI_FONT string constant to styling package

--- packages/styling/src/index.ts
+++ packages/styling/src/index.ts
@@ -1,2 +1,5 @@
 export { registerStyles, unregisterStyles, updateStyles } from './style-registry';
 export { default as Style } from './Style';
+
+export const SYSTEM_UI_FONT =
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif';
