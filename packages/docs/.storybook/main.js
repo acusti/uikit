eb64335fc5ffea4bf9caf5935b4fcda5b7878d61
Andrew Patton <andrew@acusti.ca>
2021-06-25 15:24:30 -0700

    Add @storybook/addon-docs to the storybook config addons array
    the docs addon was still somehow being used (maybe via @storybook/addon-essentials?), but this makes it explicit

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
     stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-docs',
+        '@storybook/addon-essentials',
+        '@storybook/addon-links',
+    ],
     webpackFinal: (config, { configType }) => {
         config.resolve.alias['core-js'] = path.dirname(require.resolve('core-js'));
         return config;
