54335d1650988db9536b4274a304aba715b216b5
Andrew Patton <andrew@acusti.ca>
2021-09-19 09:29:49 -0700

    Add missing extension to styling’s export from statement
    "type": "module" packages require extensions

--- packages/styling/src/index.ts
+++ packages/styling/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Style } from './Style';
+export { default as Style } from './Style.js';
 
 export const SYSTEM_UI_FONT =
     '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif';
