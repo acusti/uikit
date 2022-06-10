35e42899aca68ea36d76228331e307bbf66e2489
Andrew Patton <andrew@acusti.ca>
2022-06-10 15:28:10 -0700

    Improve post’s types by omitting competing types

--- packages/post/src/types.ts
+++ packages/post/src/types.ts
@@ -2,12 +2,12 @@ import type { RequestOptions as _RequestOptions } from 'node:https';
 
 export type FetchHeaders = { [key: string]: string };
 
-export type FetchOptionsWithBody = _RequestOptions & {
+export type FetchOptionsWithBody = Omit<_RequestOptions, 'body' | 'headers'> & {
     body: string;
     headers?: FetchHeaders;
 };
 
-export type FetchOptionsWithQuery = _RequestOptions & {
+export type FetchOptionsWithQuery = Omit<_RequestOptions, 'body' | 'headers'> & {
     body?: never;
     headers?: FetchHeaders;
     query: string;
@@ -16,4 +16,4 @@ export type FetchOptionsWithQuery = _RequestOptions & {
 
 export type FetchOptions = FetchOptionsWithBody | FetchOptionsWithQuery;
 
-export type RequestOptions = _RequestOptions & { headers: FetchHeaders };
+export type RequestOptions = Omit<_RequestOptions, 'headers'> & { headers: FetchHeaders };
