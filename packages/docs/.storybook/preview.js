0ca7de2ec2f10b4c7bd276a5018855bd52721f15
Andrew Patton <andrew@acusti.ca>
2022-10-13 13:43:02 -0700

    Default to “Docs” view and render it as first tab

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,3 +1,7 @@
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    previewTabs: {
+        'storybook/docs/panel': { index: -1 }, // move “Docs” tab before “Canvas”
+    },
+    viewMode: 'docs',
 };
