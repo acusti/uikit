0e69c5311207a70d930009d1a4f4e34c3acd50eb
Andrew Patton <andrew@acusti.ca>
2023-10-20 09:48:09 -0700

    :book: Add basic stories for DatePicker component

---
+++ packages/docs/stories/DatePicker.stories.ts
@@ -0,0 +1,56 @@
+import DatePicker from '../../date-picker/src/DatePicker.js';
+
+import './DatePicker.css';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof DatePicker> = {
+    argTypes: {
+        dateEnd: {
+            control: 'date',
+            description: '(optional) end date of current date range',
+        },
+        dateStart: {
+            control: 'date',
+            description: '(optional) start date of current date range',
+        },
+    },
+    component: DatePicker,
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    '`DatePicker` is a React component that renders a calendar-based date picker UI with support for date ranges.',
+            },
+        },
+    },
+    //https://storybook.js.org/docs/react/writing-docs/autodocs#setup-automated-documentation
+    tags: ['autodocs'],
+    title: 'UIKit/Controls/DatePicker/DatePicker',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DatePicker>;
+
+export const DefaultDatePicker: Story = {
+    args: {
+        className: 'default-date-picker-story',
+    },
+};
+
+export const TwoUpDatePicker: Story = {
+    args: {
+        className: 'two-up-date-picker-story',
+        isTwoUp: true,
+    },
+};
+
+export const DateRangeNavidadDiaDeLosReyesDatePicker: Story = {
+    args: {
+        className: 'date-range-month-calendar-story',
+        dateEnd: new Date(2024, 0, 6),
+        dateStart: new Date(2023, 11, 25),
+        isTwoUp: true,
+    },
+};
