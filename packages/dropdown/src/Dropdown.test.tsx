e2c123095ab94f69925e602170341f6628b24929
Andrew Patton <andrew@acusti.ca>
2023-12-24 09:38:32 -0800

    Prefer screen over render desctructure
    reference: https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#not-using-screen

--- packages/dropdown/src/Dropdown.test.tsx
+++ packages/dropdown/src/Dropdown.test.tsx
@@ -1,7 +1,7 @@
 // @vitest-environment happy-dom
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
+import React from 'react'; // eslint-disable-line @typescript-eslint/no-unused-vars
 import { afterEach, describe, expect, it } from 'vitest';
 
 import Dropdown from './Dropdown.js';
@@ -11,7 +11,7 @@ afterEach(cleanup);
 describe('Dropdown.js', () => {
     it('renders its contents as a dropdown menu with an empty button to trigger it when it’s rendered with a single child', async () => {
         const user = userEvent.setup();
-        const { getByRole, getByTestId } = render(
+        render(
             <Dropdown>
                 <ul data-testid="dropdown-list">
                     <li>0px</li>
@@ -28,15 +28,15 @@ describe('Dropdown.js', () => {
             </Dropdown>,
         );
 
-        const trigger = getByRole('button');
+        const trigger = screen.getByRole('button');
         expect(trigger.innerHTML).toBe('');
         await user.click(trigger);
-        expect(getByTestId('dropdown-list')).toBeTruthy();
+        expect(screen.getByTestId('dropdown-list')).toBeTruthy();
     });
 
     it('renders the first child as a trigger and the second child as a dropdown when given two children', async () => {
         const user = userEvent.setup();
-        const { getByRole, getByTestId } = render(
+        render(
             <Dropdown>
                 File
                 <ul data-testid="file-menu">
@@ -57,10 +57,10 @@ describe('Dropdown.js', () => {
             </Dropdown>,
         );
 
-        const trigger = getByRole('button');
+        const trigger = screen.getByRole('button');
         expect(trigger.innerHTML).toBe('File');
         await user.click(trigger);
-        expect(getByTestId('file-menu')).toBeTruthy();
+        expect(screen.getByTestId('file-menu')).toBeTruthy();
     });
 
     it('triggers props.onClose and props.onOpen at the appropriate times', async () => {
@@ -70,7 +70,7 @@ describe('Dropdown.js', () => {
         const handleOpen = () => openedCount++;
 
         const user = userEvent.setup();
-        const { getByRole, queryByTestId } = render(
+        render(
             <Dropdown onClose={handleClose} onOpen={handleOpen}>
                 File
                 <ul data-testid="file-menu">
@@ -91,17 +91,17 @@ describe('Dropdown.js', () => {
             </Dropdown>,
         );
 
-        const trigger = getByRole('button');
+        const trigger = screen.getByRole('button');
         expect(closedCount).toBe(0);
         expect(openedCount).toBe(0);
         await user.click(trigger);
         expect(closedCount).toBe(0);
         expect(openedCount).toBe(1);
-        expect(queryByTestId('file-menu')).toBeTruthy();
+        expect(screen.getByTestId('file-menu')).toBeTruthy();
         await user.type(trigger, '{Esc}');
         expect(closedCount).toBe(1);
         expect(openedCount).toBe(1);
-        expect(queryByTestId('file-menu')).toBe(null);
+        expect(screen.queryByTestId('file-menu')).toBe(null);
     });
 
     it('triggers props.onOpen immediately if props.isOpenOnMount', async () => {
@@ -111,18 +111,18 @@ describe('Dropdown.js', () => {
         const handleOpen = () => openedCount++;
 
         const user = userEvent.setup();
-        const { getByRole, queryByTestId } = render(
+        render(
             <Dropdown isOpenOnMount onClose={handleClose} onOpen={handleOpen}>
-                <p data-testid="dropdown">this is the dropdown contents</p>
+                <p data-testid="dropdown-body">this is the dropdown contents</p>
             </Dropdown>,
         );
 
-        const trigger = getByRole('button');
-        expect(queryByTestId('dropdown')).toBeTruthy();
+        const trigger = screen.getByRole('button');
+        expect(screen.getByTestId('dropdown-body')).toBeTruthy();
         expect(openedCount).toBe(1);
         await user.click(trigger);
         expect(closedCount).toBe(1);
         expect(openedCount).toBe(1);
-        expect(queryByTestId('dropdown')).toBe(null);
+        expect(screen.queryByTestId('dropdown-body')).toBe(null);
     });
 });
