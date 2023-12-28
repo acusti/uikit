22044915a3b615a4e1482655aa928476ebf6cd45
Andrew Patton <andrew@acusti.ca>
2023-12-28 09:24:46 -0800

    :book: Add submitOnEnter <InputText> stories

--- packages/docs/stories/InputText.stories.tsx
+++ packages/docs/stories/InputText.stories.tsx
@@ -1,8 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import * as React from 'react';
+
 import InputText from '../../input-text/src/InputText.js';
 
 import './InputText.css';
-
-import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof InputText> = {
     component: InputText,
@@ -52,11 +53,53 @@ export const InputWithInitialValueAndSelectTextOnFocus: Story = {
 export const MultiLineInputWithInitialValueAndSelectTextOnFocus: Story = {
     args: {
         className: 'multi-line-input-text',
-        initialValue: 'The Black Hawk War, or, How to Demolish an Entire Civilization and Still Feel Good About Yourself in the Morning, or, We Apologize for the Inconvenience but You’re Going to Have to Leave Now, or, “I have fought the Big Knives and will continue to fight them until they are off our lands!”',
+        initialValue:
+            'The Black Hawk War, or, How to Demolish an Entire Civilization and Still Feel Good About Yourself in the Morning, or, We Apologize for the Inconvenience but You’re Going to Have to Leave Now, or, “I have fought the Big Knives and will continue to fight them until they are off our lands!”',
         maxHeight: 600,
         multiLine: true,
         name: 'multi-line-input',
         placeholder: 'enter text of any length',
         selectTextOnFocus: true,
+    },
+};
+
+// TODO how do i wrap this in a <form onSubmit={() => {console.log('form submitted')}}>?
+const SUBMIT_ON_ENTER_PROPS = {
+    className: 'multi-line-input-text',
+    maxHeight: 600,
+    multiLine: true,
+    name: 'multi-line-submit-on-enter-input',
+    placeholder: 'enter text of any length',
+    submitOnEnter: true,
+};
+
+const formatDate = new Intl.DateTimeFormat(undefined, {
+    timeStyle: 'medium',
+}).format;
+
+export const MultiLineInputWithSubmitOnEnter: Story = {
+    args: SUBMIT_ON_ENTER_PROPS,
+    render() {
+        const [lastSubmitDate, setLastSubmitDate] = React.useState(null);
+        const lastSubmit = lastSubmitDate ? formatDate(lastSubmitDate) : 'never';
+
+        return (
+            <form
+                onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+                    event.preventDefault();
+                    setLastSubmitDate(new Date());
+                }}
+            >
+                <InputText {...SUBMIT_ON_ENTER_PROPS} />
+                <pre>Last submitted: {lastSubmit}</pre>
+            </form>
+        );
+    },
+};
+
+export const MultiLineInputWithSubmitOnEnterNoForm: Story = {
+    args: {
+        ...SUBMIT_ON_ENTER_PROPS,
+        name: SUBMIT_ON_ENTER_PROPS.name + '-no-form',
     },
 };
