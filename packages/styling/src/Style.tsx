1ea596b77badd7f01281f1d6883c3455ee79a27b
Andrew Patton <andrew@acusti.ca>
2025-01-18 12:25:10 -0800

    :memo: Update comments for <Style> component

--- packages/styling/src/Style.tsx
+++ packages/styling/src/Style.tsx
@@ -9,10 +9,9 @@ type Props = {
 };
 
 const Style = ({ children, href: _href, precedence = 'medium' }: Props) => {
-    // Minify CSS styles by replacing consecutive whitespace (including \n) with ' '
+    // Minify CSS styles to prevent unnecessary cache misses
     const { href, styles } = useStyles(children, _href);
-    // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/canary.d.ts
-    // https://react.dev/reference/react-dom/components/style#props
+
     return (
         <style href={href} precedence={precedence}>
             {styles}
