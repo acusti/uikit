9984154f92680d0a563ed9708c9ca04bbe782b6d
Andrew Patton <andrew@acusti.ca>
2025-04-09 07:16:34 -0700

    📝 Add docs reference for PnP workaround + cleanup

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -2,13 +2,9 @@ import { dirname, join } from 'path';
 
 import type { StorybookConfig } from '@storybook/react-vite';
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-    return dirname(import.meta.resolve(join(value, 'package.json'))).replace(/^file:\/\//, '');
-}
+// https://storybook.js.org/docs/faq#how-do-i-fix-module-resolution-in-special-environments
+const getAbsolutePath = (value: string): any =>
+    dirname(import.meta.resolve(join(value, 'package.json'))).replace(/^file:\/\//, '');
 
 const config: StorybookConfig = {
     addons: [getAbsolutePath('@storybook/addon-essentials')],
