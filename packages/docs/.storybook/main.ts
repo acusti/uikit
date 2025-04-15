e82f6348899ce66c4bf55222394ed97ff96ddd38
Andrew Patton <andrew@acusti.ca>
2025-04-15 16:30:33 -0700

    Try to make storybook PnP workaround work in CI

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -3,8 +3,9 @@ import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 // https://storybook.js.org/docs/faq#how-do-i-fix-module-resolution-in-special-environments
+const _require = typeof require === 'undefined' ? import.meta : require;
 const getAbsolutePath = (value: string): any =>
-    dirname(import.meta.resolve(join(value, 'package.json'))).replace(/^file:\/\//, '');
+    dirname(_require.resolve(join(value, 'package.json'))).replace(/^file:\/\//, '');
 
 const config: StorybookConfig = {
     addons: [getAbsolutePath('@storybook/addon-essentials')],
