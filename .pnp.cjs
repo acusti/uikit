c4c6a1fd99299089faa774287fdf8e1ee0b3c274
Andrew Patton <andrew@acusti.ca>
2025-06-01 15:02:25 -0700

    Add direct dependency on react-docgen
    shouldn’t be needed but just trying to prevent this error in CI (reference: https://github.com/acusti/uikit/actions/runs/15379783511/job/43269002421#step:5:219):
    
    => Failed to build the preview
    Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/home/runner/work/uikit/uikit/.yarn/cache/react-docgen-npm-8.0.0-feb9a79bb0-1c85676a0d.zip/node_modules/react-docgen/dist/config.js' imported from /home/runner/work/uikit/uikit/.yarn/cache/react-docgen-npm-8.0.0-feb9a79bb0-1c85676a0d.zip/node_modules/react-docgen/dist/main.js
        at finalizeResolution (node:internal/modules/esm/resolve:283:11)
        at moduleResolve (node:internal/modules/esm/resolve:952:10)
        at defaultResolve (node:internal/modules/esm/resolve:1188:11)
        at ModuleLoader.defaultResolve (node:internal/modules/esm/loader:642:12)
        at #cachedDefaultResolve (node:internal/modules/esm/loader:591:25)
        at ModuleLoader.getModuleJobForRequire (node:internal/modules/esm/loader:347:53)
        at new ModuleJobSync (node:internal/modules/esm/module_job:333:34)
        at ModuleLoader.importSyncForRequire (node:internal/modules/esm/loader:320:11)
        at loadESMFromCJS (node:internal/modules/cjs/loader:1371:24)
        at Module._compile (node:internal/modules/cjs/loader:1511:5)

--- .pnp.cjs
+++ .pnp.cjs
@@ -581,6 +581,7 @@ const RAW_RUNTIME_STATE =
           ["@types/react", "npm:19.1.6"],\
           ["core-js", "npm:3.42.0"],\
           ["react", "npm:19.1.0"],\
+          ["react-docgen", "npm:8.0.0"],\
           ["react-dom", "virtual:afe4e4a1c85e0b022894420d9b3e04e61e9c8c5fe86707d0599325649d85a5d5c03732ca16e1484fddaba15449239efc39b69b9e5b4963f252ae7a4aee0b9a39#npm:19.1.0"],\
           ["storybook", "virtual:62e895eaa74935c5722a4de41d4984328b771f05bc9888b3fd8210e5282d0c5555d799ab7a4041d2fcfa758b27814e74dbaddade60b7b0a4795629e4e3c125c0#npm:9.0.4"],\
           ["typescript", "patch:typescript@npm%3A5.8.3#optional!builtin<compat/typescript>::version=5.8.3&hash=5786d5"],\
